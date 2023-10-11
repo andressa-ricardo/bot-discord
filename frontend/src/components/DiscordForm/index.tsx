e78fe61f6ee56b6914fd956b83ff26f478de863c
andressa-ricardo <andressaricardo30@gmail.com>
2023-10-11 18:16:12 -0300

    chore: color change in texts and buttons

--- frontend/src/components/DiscordForm/index.tsx
+++ frontend/src/components/DiscordForm/index.tsx
@@ -20,16 +20,16 @@ const DiscordForm = () => {
           }
         }}
       >
-        <h1>Enviar mensagem para o bot</h1>
+        <h1 className=" text-xl text-white">Enviar mensagem:</h1>
         <div className="flex flex-col gap-4">
           <textarea
-            className=" border border-slate-200 w-full min-h-[8rem] text-lg p-2 rounded-md shadow-md"
+            className=" border border-slate-200 w-full min-h-[8rem] text-lg p-2 rounded-md shadow-md "
             value={message}
             placeholder="Insira aqui a mensagem"
             onChange={(e) => setMessage(e.target.value)}
           />
           <button
-            className="p-2 rounded-md text-white bg-blue-600 pl-3 pr-3"
+            className="p-2 rounded-md text-white  bg-violet-900 pl-3 pr-3"
             type="submit"
           >
             Enviar
